1b0e66688e1eee870fe7b7abccab538bf28ad08c
luiscunhacsc <luiscunha@ismt.pt>
2025-01-05 23:54:31 +0000

    Atualização de exercícios

--- Ch09 - Introdução ao Spring Data JPA/Ch09 - Exercício 04 Criação de UI em HTML para API de Gestão de Livros/livros/src/main/java/com/example/livros/repository/LivroRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.livros.repository;
-
-import com.example.livros.model.Livro;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LivroRepository extends JpaRepository<Livro, Long> {
-}
