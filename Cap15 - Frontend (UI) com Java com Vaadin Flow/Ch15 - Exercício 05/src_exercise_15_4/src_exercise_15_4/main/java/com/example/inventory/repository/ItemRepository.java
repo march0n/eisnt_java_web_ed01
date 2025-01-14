4d7f0f45b394bfbb7c9ad7faecb3fd4b42c61133
luiscunhacsc <luiscunha@ismt.pt>
2025-01-14 23:58:53 +0000

    Atualizações em exercícios de Vaadin Flow

---
+++ Cap15 - Frontend (UI) com Java com Vaadin Flow/Ch15 - Exercício 05/src_exercise_15_4/src_exercise_15_4/main/java/com/example/inventory/repository/ItemRepository.java
@@ -0,0 +1,10 @@
+package com.example.inventory.repository;
+
+import com.example.inventory.entity.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ItemRepository extends JpaRepository<Item, Long> {
+    List<Item> findByNameContainingIgnoreCase(String name);
+}
