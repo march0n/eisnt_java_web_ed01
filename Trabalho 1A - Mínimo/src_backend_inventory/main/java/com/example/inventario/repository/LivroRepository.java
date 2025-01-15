e5ff40ed8eb46fec29295e13248095dc7b39fff9
luiscunhacsc <luiscunha@ismt.pt>
2025-01-15 10:13:59 +0000

    Atualizações Estruturais - Limpeza

--- Trabalho 1A - Mínimo/src_backend_inventory/main/java/com/example/inventario/repository/LivroRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.inventario.repository;
-
-import com.example.inventario.model.Livro;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LivroRepository extends JpaRepository<Livro, Long> {
-}
